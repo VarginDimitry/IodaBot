daae488ca245b8769ebfd46d10e1376883c470c7
dmitryvargin <dimitri.vargin@yandex.ru>
2024-05-27 21:40:30 +0300

    Init

--- src/handlers/alerts.py
+++ src/handlers/alerts.py
@@ -1,11 +1,10 @@
-from datetime import datetime
-
 from aiogram import Router, F
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message
 
+import texts.alerts
 from keys import Keys
 from utils.ioda_manager import IODAManager
 from utils.parsers import datetime_msg_parser
@@ -20,45 +19,56 @@ class AlertsForm(StatesGroup):
 
 
 @alerts_router.message(Command("start"))
-async def alerts(message: Message, state: FSMContext) -> None:
-    await message.answer("Hello! Send /alerts to get alerts")
+async def alerts(message: Message) -> None:
+    await message.answer(texts.alerts.START_TEXT)
+
+
+@alerts_router.message(Command("cancel"))
+async def cancel(message: Message, state: FSMContext) -> None:
+    await state.clear()
+    await message.answer(texts.alerts.CANCEL_TEXT)
+
+
+@alerts_router.message(Command("help"))
+async def help_(message: Message) -> None:
+    await message.answer(texts.alerts.HELP_TEXT)
 
 
 @alerts_router.message(Command("alerts"))
 async def alerts(message: Message, state: FSMContext) -> None:
     await state.set_state(AlertsForm.from_time)
-    await message.answer("Send from-time like iso or timestamp")
+    await message.answer(texts.alerts.SEND_FROM_TIME_TEXT)
 
 
 @alerts_router.message(F.text, AlertsForm.from_time)
 async def get_from_time(message: Message, state: FSMContext) -> None:
     from_dt = datetime_msg_parser(message.text)
     if from_dt is None:
-        await message.answer("Wrong datetime format")
+        await message.answer(texts.alerts.WRONG_DATETIME_FORMAT_TEXT)
         return
 
     await state.update_data({Keys.FROM: int(from_dt.timestamp())})
     await state.set_state(AlertsForm.until_time)
-    await message.answer("Send until-time like iso or timestamp")
+    await message.answer(texts.alerts.SEND_UNTIL_TIME_TEXT)
 
 
 @alerts_router.message(F.text, AlertsForm.until_time)
 async def get_until_time(message: Message, state: FSMContext) -> None:
     until_dt = datetime_msg_parser(message.text)
     if until_dt is None:
-        await message.answer("Wrong datetime format")
+        await message.answer(texts.alerts.WRONG_DATETIME_FORMAT_TEXT)
         return
 
     await state.update_data({Keys.UNTIL: int(until_dt.timestamp())})
     await state.set_state(AlertsForm.country_code)
-    await message.answer("Send country code")
+    await message.answer(texts.alerts.SEND_COUNTRY_CODE_TEXT)
 
 
 @alerts_router.message(F.text, AlertsForm.country_code)
 async def finish_alerts(message: Message, state: FSMContext) -> None:
     country_code = message.text.strip()
     if country_code not in await IODAManager.get_country_codes():
-        await message.answer("Wrong country code")
+        await message.answer(texts.alerts.WRONG_COUNTRY_CODE_TEXT)
         return
 
     await state.update_data({Keys.COUNTRY_CODE: country_code})
@@ -70,12 +80,12 @@ async def finish_alerts(message: Message, state: FSMContext) -> None:
     sources = [source for source in resp.data if source.level != "normal"]
 
     if not sources:
-        await message.answer("ALL GOOD!")
+        await message.answer(texts.alerts.ALERTS_NOT_FOUND_TEXT)
         return
 
     for source in sources:
-        await message.answer(
-            f"datasource: {source.datasource}\n"
-            f"level: {source.level}\n"
-            f"time: {source.time.strftime('%Y.%m.%m %H:%M:%S')}"
-        )
+        await message.answer(texts.alerts.ALERT_TEXT_FORMAT.format(
+            datasource=source.datasource,
+            level=source.level,
+            time=source.time.strftime('%Y.%m.%m %H:%M:%S'),
+        ))
--- src/main.py
+++ src/main.py
@@ -1,23 +1,29 @@
 import asyncio
 import logging
 import sys
-from datetime import datetime
 
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.types import BotCommand
 
 from config import settings
 from fixtures import on_start_up
 from handlers import get_dispatcher
+import texts.alerts
 
 
 async def main():
     dp = get_dispatcher(storage=RedisStorage.from_url(settings.REDIS_URL))
 
     bot = Bot(settings.TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
+    await bot.set_my_commands([
+        BotCommand(command="start", description=texts.alerts.START_COMMAND),
+        BotCommand(command="help", description=texts.alerts.HELP_COMMAND),
+        BotCommand(command="alerts", description=texts.alerts.ALERTS_COMMAND),
+        BotCommand(command="cancel", description=texts.alerts.CANCEL_COMMAND),
+    ])
     await on_start_up(dp)
     await dp.start_polling(bot)
 
